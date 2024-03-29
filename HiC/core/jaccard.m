27b73bb7d14702e50a239af4cd6db867c54bd3e3
kaizheng-academic <74539350+kaizheng-academic@users.noreply.github.com>
2024-03-29 17:44:56 +0800

    0.1

--- HiC/core/jaccard.m
+++
@@ -1,8 +0,0 @@
-function jaccard_coefficient = jaccard(vector1, vector2)
-
-intersection = length(intersect(vector1, vector2));
-unionset = length(union(vector1, vector2));
-
-
-jaccard_coefficient = intersection / unionset;
-end
